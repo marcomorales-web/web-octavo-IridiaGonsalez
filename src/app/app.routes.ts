5952b67a2783c1dc0003df0634a91a3d1300f61d
MarcoMorales-web <marco.morales@itsvc.edu.mx>
2026-02-11 15:17:54 -0600

    Correcion Iridia

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Gatos } from './adopcion/gatos/gatos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'gatos', component: Gatos },
+];
